ca303593863640cd8ccbe8519737ae17281a1de9
Luis Manyari <lmanyari@hotmail.com>
2023-11-21 23:10:33 -0500

    :P~ First Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './lucho-logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to your life, there's no turning back!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://127.0.0.1:8000"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Manyari.Dev
         </a>
       </header>
     </div>
@@ -23,3 +23,45 @@ function App() {
 }
 
 export default App;
+
+
+// function Main(props) { 
+//   return <Header msg={props.msg} />; 
+// };
+
+// function Header(props) { 
+//   return ( 
+//     <div style={{ border: "10px solid whitesmoke" }}> 
+//       <h1>Header here</h1> 
+//       <Wrapper msg={props.msg} /> 
+//     </div> 
+//   ); 
+// };
+
+// function Wrapper(props) { 
+//   return ( 
+//     <div style={{ border: "10px solid lightgray" }}> 
+//       <h2>Wrapper here</h2> 
+//       <Button msg={props.msg} /> 
+//     </div> 
+//   ); 
+// };
+
+// function Button(props) { 
+//   return ( 
+//     <div style={{ border: "20px solid orange" }}> 
+//       <h3>This is the Button component</h3> 
+//       <button onClick={() => alert(props.msg)}>Click me!</button> 
+//     </div> 
+//   ); 
+// };
+
+// function App() { 
+//   return ( 
+//     <Main  
+//       msg="I passed through the Header and the Wrapper and I reached the Button component"  
+//     /> 
+//   ); 
+// }; 
+
+// export default App;
